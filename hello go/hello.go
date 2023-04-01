6cf9479c9d124b7001e25d02e266042c96c9980c
cryptoball7 <cryptoball7@gmail.com>
2023-03-31 22:48:26 -0700

    Weird text added like '>>>>>HEAD'

--- hello go/hello.go
+++ hello go/hello.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package main
 
 import (
@@ -30,4 +31,38 @@ func main() {
 	m = info2.Mode()
 	fmt.Println(err)
 	fmt.Println(m)
+=======
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"io/fs"
+)
+
+func main() {
+	fmt.Println("Hello, WOrld!")
+
+	f,err := os.CreateTemp(filepath.Dir("tempfolder"), "tempfile.txt")
+
+	fmt.Println(f)
+	fmt.Println(err)
+
+	fmt.Println(f.Name())
+
+	info, err := os.Stat(f.Name())
+	m := info.Mode()
+	fmt.Println(err)
+	fmt.Println(m)
+
+	fmt.Println(fs.FileMode(int(0600)))
+
+	os.Chmod(f.Name(), fs.FileMode(int(0600)))
+
+	info2, err := os.Stat(f.Name())
+	m = info2.Mode()
+	fmt.Println(err)
+	fmt.Println(m)
+>>>>>>> 75ce228f44ce2d92e6695d836e4d48d143617ab1
 }
