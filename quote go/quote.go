362964bf43ed18ab4c5829b034143def0bbde2ad
cryptoball7 <cryptoball7@gmail.com>
2023-04-02 00:00:34 -0700

    added comment with the output

--- quote go/quote.go
+++ quote go/quote.go
@@ -5,5 +5,5 @@ import "fmt"
 import "rsc.io/quote"
 
 func main() {
-	fmt.Println(quote.Go())
+	fmt.Println(quote.Go()) // Output: Don't communicate by sharing memory, share memory by communicating.
 }
