90fa8ce20dddb9bde1d84ec65d88388194a0cf65
cryptoball7 <cryptoball7@gmail.com>
2023-04-22 12:44:26 -0700

    flask program says Hello, name!

---
+++ 10 flask hello 2/flask_hello.py
@@ -0,0 +1,17 @@
+# Following flask tutorial:
+# https://flask.palletsprojects.com/en/2.2.x/quickstart/
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+	return "<p>Hello, World!</p>"
+
+from markupsafe import escape
+
+@app.route("/<name>")
+
+def hello(name):
+	return f"Hello, {escape(name)}!"
